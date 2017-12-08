674e9d324734d290ea815a24d32b18f9684885fb
Juan Candia <jcandia2@yahoo.com>
2017-12-08 12:10:47 -0600

    add migration file for albums

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(100) DEFAULT 'NONE',
+  name VARCHAR(50) NOT NULL,
+  release_date INT NOT NULL,
+  sales FLOAT DEFAULT 0.0,
+  genre VARCHAR(50),
+  PRIMARY KEY (id)
+);
+
