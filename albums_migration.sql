fdbe249dfad952db8c76906c48c94b601d690314
Juan Candia <jcandia2@yahoo.com>
2017-12-08 15:01:23 -0600

    add albums_seeder.sql to insert 1 row of values, added NOT NULL to albums and pizza_orders tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,14 +1,16 @@
 USE codeup_test_db;
 
+-- Create albums table --
+-- "Migrations" are files that create, modify, or delete tables --
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100) DEFAULT 'NONE',
+  artist VARCHAR(100) NOT NULL DEFAULT 'NONE',
   name VARCHAR(50) NOT NULL,
   release_date INT NOT NULL,
-  sales FLOAT DEFAULT 0.0,
-  genre VARCHAR(50),
+  sales FLOAT NOT NULL DEFAULT 0.0,
+  genre VARCHAR(50) NOT NULL ,
   PRIMARY KEY (id)
 );
 
---
+++ albums_seeder.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+
+-- create seeds for table, base data --
+INSERT INTO albums (artist, name, release_date, sales, genre)
+    VALUES ('Michael Jackson', 'Thriller', 1982, 66000000, 'Pop, Rock, R&B');
