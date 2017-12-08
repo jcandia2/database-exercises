8ab0d154fccd046b241e413cd5ddba879306cf32
Juan Candia <jcandia2@yahoo.com>
2017-12-08 13:56:37 -0600

    add pizza_orders_migration.sql file

---
+++ pizza_orders_migration.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS pizza_orders;
+
+CREATE TABLE pizza_orders (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  orderer_name VARCHAR(100) NOT NULL,
+  orderer_phone_number CHAR(10) DEFAULT 'NONE',
+  orderer_email CHAR(30) DEFAULT 'NONE',
+  delivery_address VARCHAR(1000),
+  pizza_size CHAR(10) NOT NULL,
+  order_cost DECIMAL(13,2),
+  order_description CHAR(100),
+  delivered TINYINT DEFAULT '0',
+  time_of_order DATETIME NOT NULL,
+  time_of_delivery DATETIME,
+  PRIMARY KEY (id)
+);
