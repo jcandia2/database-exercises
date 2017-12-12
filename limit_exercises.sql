8917a4f17220e7ecfae40514a828239fb6b9487f
Juan Candia <jcandia2@yahoo.com>
2017-12-12 14:41:00 -0600

    update limit_exercises after Fer's solutions

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -12,6 +12,7 @@ ORDER BY birth_date, hire_date desc
 limit 5;
 
 # to display nth page of q results do n * q - q for offset
+# limit * (offset -1)
 SELECT first_name, last_name
 FROM employees
 WHERE hire_date LIKE '199%' and birth_date like '%12-25'
